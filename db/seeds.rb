64d43237e3c64999ab5fdcbdcb4aca8d7f56e06b
korosdennis <deniptoo98@gmaiil.com>
2022-10-08 15:10:59 +0300

    sample seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ puts " Seeding Heroes..."
+ Hero.create!([
+  {name:"Peter Parker", supername:"Spider-Man"},
+  {name:"Steve Rogers", supername:"Captain America"},
+  {name:"Bruce Banner", supername:"Hulk"},
+  {name:"Natasha Romanoff", supername:"Black Widow"},
+  {name:"Matthew Murdock", supername:"Dare Devil"},
+  {name:"Reed Richards", supername:"Mr.Fantastic"},
+  {name:"T'Challa", supername:"Black Panther"}
+ ])
+
+ puts "seeding Powers"
+  Power.create!([
+    {name:"super strength",description: "gives the wielder super human strengths"},
+    {name:"flight",description: "gives the wielder the ability to fly through the skys at supersonic speed"},
+    {name:"super human senses",description: "allows the person to user their senses at super human level"},
+    {name:"elasticity",description: "can stretch the human body to extreme lengths"}
+  ])
